27e57558fc2953856aebef44ec5fae232e5dc3b1
Valeriia Isaienko <93624578+Magic1vy@users.noreply.github.com>
2023-04-21 13:24:40 -0400

    Update Cart.js

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -2,17 +2,26 @@ import { useSelector } from "react-redux";
 import CartItem from "./CartItem";
 import { getCartItems, getTotalPrice } from "../redux/cartSlice";
 
-const Cart = () =>{
-    
-    const cartItems = useSelector(getCartItems);
-    const totalPrice = useSelector(getTotalPrice);
+const Cart = () => {
+  const cartItems = useSelector(getCartItems);
+  const totalPrice = useSelector(getTotalPrice);
 
-    return (
+  return (
     <div className="cartComponent">
+      {cartItems.length === 0 ? (
         <div className="cart-items">
-       {  cartItems.map(cartItem => <CartItem key={cartItem.id}  cartItem={cartItem} />)}
-       <h3 className='headingThree' >TOTAL: ${totalPrice}</h3>
-       </div>
-    </div>)
-}
+        <h3 className="headingThree">Your cart is empty</h3>
+        </div>
+      ) : (
+        <div className="cart-items">
+          {cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} cartItem={cartItem} />
+          ))}
+          <h3 className="headingThree">TOTAL: ${totalPrice}</h3>
+        </div>
+      )}
+    </div>
+  );
+};
+
 export default Cart;
